b3cd09e89c97a382753b466850f998322e783c44
Arve Hjønnevåg <arve@android.com>
2019-02-15 15:57:05 -0800

    test-runner: Add initrd support
    Needed to boot hafnium
    
    Bug: 122357282
    Change-Id: I737935228bc6c211b15ed35090fdb079a451d418

--- test-runner/arm64/boot.c
+++ test-runner/arm64/boot.c
@@ -24,6 +24,7 @@
 
 #include <test-runner-arch.h>
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -31,9 +32,13 @@
 #define FW_CFG_BASE 0x09020000
 #define DTB_ADDR 0x40000000
 #define LOAD_ADDR 0x40080000
+#define INITRD_ADDR 0x48000000
 
 #define FW_CFG_KERNEL_SIZE 0x08
 #define FW_CFG_KERNEL_DATA 0x11
+
+#define FW_CFG_INITRD_SIZE 0x0b
+#define FW_CFG_INITRD_DATA 0x12
 
 #define FW_CFG_DMA_CTL_READ 0x02
 #define FW_CFG_DMA_CTL_SELECT 0x08
@@ -60,7 +65,7 @@ static uint64_t rev64(uint64_t val) {
     return rev(val, 64);
 }
 
-void boot_next(void) {
+static bool load_image(uint64_t addr, uint16_t cfg_data, uint16_t cfg_size) {
     volatile uint32_t* cfg_data32 = (void*)FW_CFG_BASE;
     volatile uint16_t* cfg_ctl = (void*)(FW_CFG_BASE + 0x8);
     volatile uint64_t* cfg_dma = (void*)(FW_CFG_BASE + 0x10);
@@ -70,7 +75,6 @@ void boot_next(void) {
         uint32_t length;
         uint64_t address;
     } dma;
-    typedef void (*entry_func_t)(uint64_t dtb_paddr);
 
     /*
      * Setup dma description to select FW_CFG_KERNEL_DATA item and read from it.
@@ -79,32 +83,42 @@ void boot_next(void) {
      *
      * Interface is defined in external/qemu/hw/nvram/fw_cfg.c.
      */
-    dma.control = rev32(FW_CFG_DMA_CTL_READ | FW_CFG_DMA_CTL_SELECT |
-                        FW_CFG_KERNEL_DATA << 16);
+    dma.control =
+            rev32(FW_CFG_DMA_CTL_READ | FW_CFG_DMA_CTL_SELECT | cfg_data << 16);
 
     /*
      * Select FW_CFG_KERNEL_SIZE item and copy it to the dma lenght descriptor.
      * Both are big-endian so byte order reversal is needed.
      */
-    *cfg_ctl = rev16(FW_CFG_KERNEL_SIZE);
+    *cfg_ctl = rev16(cfg_size);
     dma.length = rev32(*cfg_data32);
     if (!dma.length) {
         /* Return if no image was provided */
-        return;
+        return false;
     }
 
     /*
      * Set the target address for the DMA. Reverse the byte order of the address
      * since the dma descriptor expects big-endian byte order.
      */
-    dma.address = rev64(LOAD_ADDR);
+    dma.address = rev64(addr);
 
     /* Start the dma */
     *cfg_dma = rev64((uint64_t)&dma);
     if (dma.control != 0) {
         /* Return if dma was not successful */
+        return false;
+    }
+    return true;
+}
+
+void boot_next(void) {
+    typedef void (*entry_func_t)(uint64_t dtb_paddr);
+
+    if (!load_image(LOAD_ADDR, FW_CFG_KERNEL_DATA, FW_CFG_KERNEL_SIZE)) {
         return;
     }
+    load_image(INITRD_ADDR, FW_CFG_INITRD_DATA, FW_CFG_INITRD_SIZE);
 
     /* Jump to the image we just loaded with the dtb address in x0 */
     ((entry_func_t)LOAD_ADDR)(DTB_ADDR);
